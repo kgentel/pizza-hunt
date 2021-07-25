94c0a4621fcbf09987fdb0c07fde90711c8c9d7d
kgentel <katie.m.gentel@gmail.com>
2021-07-24 19:19:50 -0500

    added mongoose validation

--- models/Comment.js
+++ models/Comment.js
@@ -10,9 +10,12 @@ const ReplySchema = new Schema(
     },
     replyBody: {
       type: String,
+      required: true,
+      trim: true
     },
     writtenBy: {
       type: String,
+      required: true,
     },
     createdAt: {
       type: Date,
@@ -32,9 +35,11 @@ const CommentSchema = new Schema(
   {
     writtenBy: {
       type: String,
+      required: true,
     },
     commentBody: {
       type: String,
+      required: true,
     },
     createdAt: {
       type: Date,
